a1c347682ba8b29303f65436a8b51f689b7cf11b
Aman Dhurwey <2017030@iiitdmj.ac.in>
2019-11-07 01:46:09 +0530

    ready to deploy

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const PORT = 3000;
+const app = express();
+
+app.get('/', (req,res)=>{
+    res.send('This is going to be awesome');
+});
+
+app.listen(PORT, ()=>{
+    console.log("server is running");
+});
