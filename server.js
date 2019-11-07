f2c3fcd4550a68719ff6427bca596b84aeaa1b5a
Aman Dhurwey <2017030@iiitdmj.ac.in>
2019-11-08 00:05:14 +0530

    make it better

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+
 app.get('/', (req,res)=>{
-    res.send('This is going to be awesome');
+   res.sendFile(path.join(__dirname, "public","index.html"));
 });
 
+
+ 
+
 app.listen(PORT, ()=>{
-    console.log("server is running");
+    console.log("server is running on ");
 });
